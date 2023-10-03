59cec68262ebbb19d4bad9b7c8f5286c1897ff24
vuwase <v.uwase@alustudent.com>
2023-10-03 10:05:31 -0700

    Social-media_usernames.py

---
+++ Social-media_usernames.py
@@ -0,0 +1,9 @@
+#!/bin/python3 
+
+
+#  Social media usernames
+
+username_string = "@LaVILLA007"
+username = re.search(r'@([a-zA-Z0-9_]+)', username_string)
+if username:
+    print("Username:", username.group(1))
