1dfed88dc688e2cde1a870e954b0f7212677714b
m-mwangi <m.mwangi2@alustudent.com>
2023-10-03 10:46:56 -0700

    Product_codes.py

---
+++ Product_codes.py
@@ -0,0 +1,8 @@
+# marion (Product codes)
+product_code = "007LAVILLA"
+pattern = r'^\d{3}[A-Z]+'
+
+if re.match(pattern, product_code):
+    print("Valid Product Code")
+else:
+    print("Invalid Product Code")
