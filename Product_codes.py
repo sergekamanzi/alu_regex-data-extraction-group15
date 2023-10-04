caddddfcc7d0d76f270883361e5bd1e1e78c73ab
m-mwangi <m.mwangi2@alustudent.com>
2023-10-04 03:56:38 -0700

    Product_codes.py

--- Product_codes.py
+++ Product_codes.py
@@ -1,3 +1,9 @@
+#!/bin/python3
+# product code 
+
+import re
+
+
 product_code = "007LAVILLA"
 pattern = r'^\d{3}[A-Z]+'
 
@@ -5,3 +11,5 @@ if re.match(pattern, product_code):
     print("Valid Product Code")
 else:
     print("Invalid Product Code")
+    
+print("007LAVILLA")
