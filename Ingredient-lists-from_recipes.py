aeb7b00f9762dbc46168c0ae53aafb234174ee92
IkireziI <i.ikirezialustudent.com>
2023-10-04 02:57:10 -0700

    recipes

--- Ingredient-lists-from_recipes.py
+++ Ingredient-lists-from_recipes.py
@@ -1,7 +1,9 @@
 #!/bin/python3
 
 
-# Ingredient lists from recipes
-    ingredient_list = "water,vegetable oil,honey,steak,green onions,ginger,baking soda,pepper"
+import re
+
+# Ingredient list from recipes
+ingredient_list = "water,vegetable oil,honey,steak,green onions,ginger,baking soda,pepper"
 ingredients = re.split(r',\s*', ingredient_list)
 print("Ingredients:", ingredients)
